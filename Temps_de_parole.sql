3ba7a3f0edffe2e8a9f77b8b53f58e401f049495
Robin Biagioni <robinbiagioni07@gmail.com>
2021-03-01 23:53:46 +0100

    modificationde register.php + création de registration.php : l'inscription sur le site est opérationnelle ++ base 3.0 fautes en moins

--- Temps_de_parole.sql
+++ Temps_de_parole.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: Mar 01, 2021 at 09:30 PM
+-- Generation Time: Mar 01, 2021 at 10:50 PM
 -- Server version: 5.7.26
 -- PHP Version: 7.4.2
 
@@ -17,7 +17,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `temps_paroles`
+-- Database: `Temps_de_parole`
 --
 
 -- --------------------------------------------------------
@@ -901,9 +901,16 @@ CREATE TABLE `PAGE` (
 CREATE TABLE `UTILISATEUR` (
   `idUtilisateur` int(11) NOT NULL,
   `pseudo` varchar(5000) DEFAULT NULL,
-  `mpd_u` varchar(5000) DEFAULT NULL,
+  `mdp_u` varchar(5000) DEFAULT NULL,
   `mail_utilisateur` varchar(5000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `UTILISATEUR`
+--
+
+INSERT INTO `UTILISATEUR` (`idUtilisateur`, `pseudo`, `mdp_u`, `mail_utilisateur`) VALUES
+(1, 'Robin', 'a', 'rr@rr.fr');
 
 --
 -- Indexes for dumped tables
@@ -992,7 +999,7 @@ ALTER TABLE `COMMENTAIRE`
 -- AUTO_INCREMENT for table `UTILISATEUR`
 --
 ALTER TABLE `UTILISATEUR`
-  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idUtilisateur` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Constraints for dumped tables
