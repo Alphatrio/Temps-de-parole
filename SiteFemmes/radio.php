d0850a8ee21e47747a0cfde90bce37d111e9615a
Visonneau Louis <louisvisonneau@icloud.com>
2021-03-01 13:38:29 +0100

    début pages

---
+++ SiteFemmes/radio.php
@@ -0,0 +1,66 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+  <title>Les Femmes à la TV et à la Radio</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+  <link rel="stylesheet" href="styles/style.css" type="text/css" media="screen" />
+  
+
+</head>
+
+<body>
+	<div class="sticky-top">
+		<div class = "container-fluid center">
+			<h1 class="text-center" >Les Femmes à la TV et à la Radio</h1>
+		</div>
+	</div>
+	
+	
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark xs">
+			<a class="navbar-brand" href="#">Navigation</a>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+			<div class="navbar-nav">
+				<a class="nav-item nav-link active" href="index.php">Home <span class="sr-only"></span></a>
+				<a class="nav-item nav-link" href="radio.php">Radio</a>
+				<a class="nav-item nav-link" href="tele.php">Télévision</a>
+				<a class="nav-item nav-link disabled" href="propos.php">À propos</a>
+			</div>
+		</div>
+	</nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
