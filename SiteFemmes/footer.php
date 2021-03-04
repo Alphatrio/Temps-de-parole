59e41eb238a209a6761a5a217d853fed26dca9a0
Visonneau Louis <louisvisonneau@icloud.com>
2021-03-04 16:06:32 +0100

    Amélioration visuelle de year pour présentation

--- SiteFemmes/footer.php
+++ SiteFemmes/footer.php
@@ -1,16 +1,16 @@
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
 	<!-- Fixed Footer  -->
 	<footer class="bg-dark text-center text-white">
 	  <!-- Grid container -->
 	  <div class="container p-4">
 		<!-- Section: Social media -->
 		<section class="mb-4">
-		 
+
 		  <a class="btn btn-outline-light btn-floating m-1" href="https://trello.com/b/RjvxRq7a/projet-temps-de-parole" role="button"
 			><i class="fa fa-trello"></i
 		  ></a>
@@ -25,21 +25,21 @@
 			><i class="fab fa-discord"></i
 		  ></a>
 
-		  
+
 		  <!-- Github -->
 		  <a class="btn btn-outline-light btn-floating m-1" href="https://github.com/Alphatrio/Temps-de-parole" role="button"
 			><i class="fa fa-github"></i
 		  ></a>
 		</section>
-		
+
 		<section>
-			<a class="btn btn-outline-light btn-floating m-1" href="register.php" role="button"
+			<a class="btn btn-outline-light btn-floating m-1" href="login.php" role="button"
 			><i class="fa fa-sign-in"></i
 		  >   Se connecter</a>
-		
-		
+
+
 		</section>
-		
+
 		<!-- Section: Social media -->
 
 		<!-- Section: Form -->
@@ -47,28 +47,18 @@
 		  <form action="register.php">
 			<!--Grid row-->
 			<div class="row d-flex justify-content-center">
-			  <!--Grid column-->
-			  <div class="col-auto">
-				<p class="pt-2">
-				  <strong>Inscrivez vous</strong>
-				</p>
-			  </div>
-			  <!--Grid column-->
 
 			  <!--Grid column-->
-			  <div class="col-md-5 col-12">
+			  <div class="col-md-0 col-12">
 				<!-- Email input -->
-				<div class="form-outline form-white sm-4">
-				  <input type="email" id="form5Example2" class="form-control" />
-				  <label class="form-label" for="form5Example2">Identifiant</label>
-				</div>
+
 			  </div>
 			  <!--Grid column-->
 
 			  <!--Grid column-->
 			  <div class="col-auto">
 				<!-- Submit button -->
-				<button type="submit" class="btn btn-outline-light mb-4">
+				<button type="submit" class="btn btn-secondary mb-4">
 				  S'inscrire
 				</button>
 			  </div>
@@ -85,9 +75,9 @@
 			<I>Le but de ce site est informatif, les données viennent d'<a href="https://www.data.gouv.fr/en/datasets/temps-de-parole-des-hommes-et-des-femmes-a-la-television-et-a-la-radio/#_">ici</a>, son traitement et sa mise en forme ont étés faits dans le contexte universitaire.</I>
 		  </p>
 		</section>
-		
 
-		
+
+
 
 	  <!-- Copyright -->
 	  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
@@ -97,5 +87,3 @@
 	  <!-- Copyright -->
 	</footer>
 	<!-- Footer -->
-	
-	
--- SiteFemmes/year.php
+++ SiteFemmes/year.php
@@ -12,60 +12,63 @@
 
 
 
-      <h2><?php echo $_GET['annee']; ?></h2>
-
-      <!-- TABLEAU ELUES PAR ANNEE -->
-      <table>
-        <thead>
-          <tr>
-            <th>Type d'élues</th>
-            <th>% femmes</th>
-            <th>% hommes</th>
-          </tr>
-        </thead>
-              <?php
-                $bdd = getBD_TDP();
-                $annee=$_GET['annee'];
-                $rep = $bdd->query('SELECT typeElues, repartitionElues FROM ELUES WHERE ELUES.annee =' .$annee /* formulaire */);
-                $percentage = 100;
-                while ($ligne = $rep ->fetch()) {
-                  echo '<tr>';
-                  // if ($rep == "") {
-                  //   $message == "Désolé, il n'existe pas de données pour cette année et/ou catégorie";
-                  //   echo $message;
-                  // else {
-                  echo '<td>'.$ligne['typeElues'].'</td>';
-                  // }
-                  echo '<td>'.$ligne['repartitionElues'].' %</td>';
-                  echo '<td>'.($percentage - $ligne['repartitionElues']).' %</td>';
-                  echo '</tr>';
-                }
-                $rep ->closeCursor();
-              ?>
-        </table><br>
-
-        <!-- TABLEAU ECART SALARIAL PAR ANNEE  -->
-        <table>
-          <thead>
+      <h2 class="display-4 text-center"><?php echo $_GET['annee']; ?></h2>
+    <div class="container text-center addalinea">
+        <!-- TABLEAU ELUES PAR ANNEE -->
+        <table class="table">
+          <thead class="thead-dark" >
             <tr>
-              <th>Écart salarial dans le privé</th>
-              <th>Écart salarial dans le public</th>
+              <th scope="col">Type d'élues</th>
+              <th scope="col">% femmes</th>
+              <th scope="col">% hommes</th>
             </tr>
           </thead>
                 <?php
                   $bdd = getBD_TDP();
-                  $rep = $bdd->query('SELECT EcartPrive, EcartPublic FROM Esalaire WHERE Esalaire.annee =' .$annee/* formulaire */);
+                  $annee=$_GET['annee'];
+                  $rep = $bdd->query('SELECT typeElues, repartitionElues FROM ELUES WHERE ELUES.annee =' .$annee /* formulaire */);
+                  $percentage = 100;
                   while ($ligne = $rep ->fetch()) {
                     echo '<tr>';
-                    echo '<td>'.$ligne['EcartPrive'].' %</td>';
-                    echo '<td>'.$ligne['EcartPublic'].' %</td>';
+                    // if ($rep == "") {
+                    //   $message == "Désolé, il n'existe pas de données pour cette année et/ou catégorie";
+                    //   echo $message;
+                    // else {
+                    echo '<td>'.$ligne['typeElues'].'</td>';
+                    // }
+                    echo '<td>'.$ligne['repartitionElues'].' %</td>';
+                    echo '<td>'.($percentage - $ligne['repartitionElues']).' %</td>';
                     echo '</tr>';
                   }
                   $rep ->closeCursor();
                 ?>
           </table><br>
 
+          <!-- TABLEAU ECART SALARIAL PAR ANNEE  -->
+          <table class="table">
+            <thead class="thead-dark">
+              <tr>
+                <th scope="col">Écart salarial dans le privé</th>
+                <th scope="col">Écart salarial dans le public</th>
+              </tr>
+            </thead>
+                  <?php
+                    $bdd = getBD_TDP();
+                    $rep = $bdd->query('SELECT EcartPrive, EcartPublic FROM Esalaire WHERE Esalaire.annee =' .$annee/* formulaire */);
+                    while ($ligne = $rep ->fetch()) {
+                      echo '<tr>';
+                      echo '<td>'.$ligne['EcartPrive'].' %</td>';
+                      echo '<td>'.$ligne['EcartPublic'].' %</td>';
+                      echo '</tr>';
+                    }
+                    $rep ->closeCursor();
+                  ?>
+            </table><br>
+    </div>
 
+
+
+          
     <?php require_once "./footer.php";?>
 
 
