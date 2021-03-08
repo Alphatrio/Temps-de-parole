e8d8b5867f8fb4e31340242e9bfab78a9e853699
Theodore Michel-Picque <theodore.michelpicque@gmail.com>
2021-03-08 11:46:29 +0100

    changement de la connexion pour la bd

--- SiteFemmes/call_bd.php
+++ SiteFemmes/call_bd.php
@@ -1,6 +1,6 @@
 <?php
       function getBD_TDP(){
-        $bdd = new PDO('mysql:host=localhost;dbname=Temps_de_parole;charset=utf8', 'root', 'root');
+        $bdd = new PDO('mysql:host=sql4.cluster1.easy-hebergement.net;dbname=tempsdeparole;charset=utf8', 'tempsdeparole', 'alphatrio');
         return $bdd;
       }
 ?>
