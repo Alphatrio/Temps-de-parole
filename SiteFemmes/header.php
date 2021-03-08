47650ddb91192fdb24619468577bc9f09060fa3f
LazareisSoaked <louisvisonneau@icloud.com>
2021-03-08 14:41:39 +0100

    No navbar expand when width < 1000px

--- SiteFemmes/header.php
+++ SiteFemmes/header.php
@@ -5,25 +5,32 @@
 </div>
 	
 	
-	<nav class="navbar navbar-expand-sm sticky-top navbar-dark bg-dark">
-			<a class="navbar-brand" href="#top">
-				<?php
-					// $link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-					// //echo $link;
-					// echo substr($link,28,-4);
-				?>
-				Navigation
-				</a>
+	<nav class="navbar <?php if($_SESSION['screen_width']>=1000){echo "navbar-expand-sm"; }else{ echo "navbar";} ?> sticky-top navbar-dark bg-dark">
+		<div class="container-fluid">
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
-		<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-			<div class="navbar-nav">
-				<a class="nav-item nav-link active" href="index.php">Home <span class="sr-only"></span></a>
-				<a class="nav-item nav-link" href="radio.php">Radio</a>
-				<a class="nav-item nav-link" href="tele.php">Télévision</a>
-				<a class="nav-item nav-link" href="register.php" role="button"><i class="fa fa-sign-in"></i>      S'inscrire/Se connecter</a>
-				
+			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" href="index.php">Home <span class="sr-only"></span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="radio.php">Radio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="tele.php">Télévision</a>
+					</li>
+				</ul>
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item">
+						<a class="nav-link " href="register.php" role="button"><i class="fa fa-sign-in"></i>      S'inscrire</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="login.php" role="button"><i class="fa fa-sign-in"></i>      Se connecter</a>
+					</li>
+				</ul>
+				</div>
 			</div>
 		</div>
 	</nav>
