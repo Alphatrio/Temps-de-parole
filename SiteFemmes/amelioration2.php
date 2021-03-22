9e9a224003c6863a2cb51ee1ca6c0976cc5c0668
Theodore Michel--Picque <theodore.michelpicque@gmail.com>
2021-03-22 16:13:48 +0100

    ajout des feedback

---
+++ SiteFemmes/amelioration2.php
@@ -0,0 +1,35 @@
+<?php
+       session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Les Femmes à la TV et à la Radio</title>
+  <?php require_once "./appelstyles.php";
+
+  	require 'call_bd.php';
+
+  	function feedBack($ame){
+        $bdd = getBD_TDP();
+        $query = "INSERT INTO amelioration (amelioration_texte) VALUES (?)";
+        $data = array($ame);
+        $statement = $bdd->prepare($query); 
+        $exec = $statement->execute($data);
+    }
+
+    feedBack($_POST['ame']);
+    echo '<meta http-equiv="Refresh" content="0; URL=index.php"/>';
+			
+            
+        ?>
+	</head>
+	
+	<body>
+		
+		
+	</body>
+	
+	
+	
+</html>
