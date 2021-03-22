6d11d60e88fab5c44b764f242fea5cbce1507992
Visonneau Louis <louisvisonneau@icloud.com>
2021-03-22 13:31:16 +0100

    changment boutons et autres

--- SiteFemmes/index.php
+++ SiteFemmes/index.php
@@ -24,33 +24,32 @@
 
 	<?php require_once "./header.php";?>
 
-	<!-- <div class= "container addalinea"> -->
-
-  <div class="list-group text-center">
-    <div class="list-group-item list-group-item-action active bg-info border-0 rounded-0">
-        <tr>
-          <div class="list-group-item list-group-item-action active disabled bg-info border-0">Années</div>
-        </tr>
-    </div>
-
-            <?php
-              $bdd = getBD_TDP();
-              $rep = $bdd->query('SELECT annee FROM Esalaire ORDER BY annee ASC');
-              while ($ligne = $rep ->fetch()) {
-                echo '<tr>';
-                echo '<a class="list-group-item list-group-item-action" href="year.php?annee='.$ligne['annee'].'">'.$ligne['annee'].'</a>';
-                echo '</tr>';
-              }
-              $rep ->closeCursor();
-
-
-            ?>
+  <div class = "maindiv">
+    <div class="list-group text-center">
+      <div class="list-group-item list-group-item-action active bg-info border-0 rounded-0">
+          <tr>
+            <div class="list-group-item list-group-item-action active disabled bg-info border-0">Années</div>
+          </tr>
       </div>
 
+              <?php
+                $bdd = getBD_TDP();
+                $rep = $bdd->query('SELECT annee FROM Esalaire ORDER BY annee ASC');
+                while ($ligne = $rep ->fetch()) {
+                  echo '<tr>';
+                  echo '<a class="list-group-item list-group-item-action" href="year.php?annee='.$ligne['annee'].'">'.$ligne['annee'].'</a>';
+                  echo '</tr>';
+                }
+                $rep ->closeCursor();
+
+
+              ?>
+        </div>
 
 
 
-	<!-- </div> -->
+
+	</div>
 
 	<?php require_once "./footer.php";?>
 
