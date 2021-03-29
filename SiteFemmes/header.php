4fd441c9741c54759768a8c4fb2280bdcdc16290
robin80577 <robinbiagioni97@gmail.com>
2021-03-29 20:22:54 +0200

    ébauche test de mettre une image header

--- SiteFemmes/header.php
+++ SiteFemmes/header.php
@@ -3,6 +3,15 @@
 			<h1 class="text-center display-4">Les Femmes à la TV et à la Radio</h1>
 		</div>
 </div>
+<!-- test en cours : image dans le header-->
+<!-- <div class=" rounded-0 jumbotron text-dark" style="margin:0px;padding:0px;height:200px;">
+	<div style="position:absolute;padding:0px;height:200px;margin-left: auto;margin-right: auto;">
+		 <img src="images/e.jpeg" style="position:relative;max-width:100%;height:100%;object-fit: cover;">
+	</div>
+		<div class = "container-fluid border-bottom-0" style="position:absolute;padding-top:70px">
+			<h1 class="text-center display-4">Les Femmes à la TV et à la Radio</h1>
+		</div>
+</div> -->
 
 
 	<nav class="navbar <?php if($_SESSION['screen_width']>=1000){echo "navbar-expand-sm"; }else{ echo "navbar";} ?> sticky-top navbar-dark bg-dark">
